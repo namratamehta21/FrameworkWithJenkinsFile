a81dadd3739a4d8f97784f92888de3b3cd98fe8b
Sandeep Kumar <skumar@kingsleygate.com>
2025-06-05 21:09:50 +0000

    HooksClass.java edited online with Bitbucket

--- src/test/java/TestComponents/HooksClass.java
+++ src/test/java/TestComponents/HooksClass.java
@@ -23,59 +23,71 @@ public class HooksClass
     LoginPage LP;
 
     public WebDriver initializeDriver() throws IOException
-    {
+{
+    Properties prop = new Properties();
+    FileInputStream FIS=new FileInputStream(System.getProperty("user.dir")
+            +"//src//main//java//Resources//Global.properties");
+    prop.load(FIS);
+    String browser = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
+    String URL=prop.getProperty("url");
+    String headlessFlag = System.getProperty("headless") != null
+            ? System.getProperty("headless")
+            : prop.getProperty("headless");
+    boolean isHeadless = headlessFlag.equalsIgnoreCase("true");
 
-        Properties prop = new Properties();
-        FileInputStream FIS=new FileInputStream(System.getProperty("user.dir")
-                +"//src//main//java//Resources//Global.properties");
-        prop.load(FIS);
-        String browser = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
-        String URL=prop.getProperty("url");
-        String headlessFlag = System.getProperty("headless") != null
-                ? System.getProperty("headless")
-                : prop.getProperty("headless");
-        boolean isHeadless = headlessFlag.equalsIgnoreCase("true");
+    if (browser.equalsIgnoreCase("chrome")) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
 
-        if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
-            if (isHeadless) {
-                options.addArguments("--headless=new"); // or "--headless" for older versions
-                options.addArguments("--window-size=1920,1080");
-                options.addArguments("--disable-gpu");
-            }
-            driver = new ChromeDriver(options);
+        // Add unique user-data-dir to avoid conflict
+        String userDataDir = "/tmp/selenoid-chrome-user-data-" + System.currentTimeMillis();
+        options.addArguments("--user-data-dir=" + userDataDir);
 
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            FirefoxOptions options = new FirefoxOptions();
-            if (isHeadless) {
-                options.addArguments("--headless");
-                options.addArguments("--width=1920");
-                options.addArguments("--height=1080");
-            }
-            driver = new FirefoxDriver(options);
-
-        } else if (browser.equalsIgnoreCase("edge")) {
-            WebDriverManager.edgedriver().setup();
-            EdgeOptions options = new EdgeOptions();
-            if (isHeadless) {
-                options.addArguments("headless");
-                options.addArguments("disable-gpu");
-                options.addArguments("window-size=1920,1080");
-            }
-            driver = new EdgeDriver(options);
-
-        } else {
-            throw new RuntimeException("Unsupported browser: " + browser);
+        // Add headless and other flags if needed
+        if (isHeadless) {
+            options.addArguments("--headless=new"); // or just "--headless" depending on your ChromeDriver version
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-gpu");
         }
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().setSize(new Dimension(1920, 1080));
-        driver.get(URL);
-        System.out.println("Selected browser: " + browser);
-        return driver;
+        // Linux-friendly options for stability on Jenkins
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+
+    } else if (browser.equalsIgnoreCase("firefox")) {
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        if (isHeadless) {
+            options.addArguments("--headless");
+            options.addArguments("--width=1920");
+            options.addArguments("--height=1080");
+        }
+        driver = new FirefoxDriver(options);
+
+    } else if (browser.equalsIgnoreCase("edge")) {
+        WebDriverManager.edgedriver().setup();
+        EdgeOptions options = new EdgeOptions();
+        if (isHeadless) {
+            options.addArguments("headless");
+            options.addArguments("disable-gpu");
+            options.addArguments("window-size=1920,1080");
+        }
+        driver = new EdgeDriver(options);
+
+    } else {
+        throw new RuntimeException("Unsupported browser: " + browser);
     }
+
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    //driver.manage().window().maximize();
+    driver.manage().window().setSize(new Dimension(1920, 1080));
+    driver.get(URL);
+
+    System.out.println("Selected browser: " + browser);
+    return driver;
+}
 
     public LoginPage launchApp() throws IOException
 
