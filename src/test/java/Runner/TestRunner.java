c040724106788e856230d60d226ebe600361efc6
Sandeep Kumar <skumar@kingsleygate.com>
2025-06-08 20:02:03 +0530

    testrunner updated

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -4,16 +4,18 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions
+@CucumberOptions(
+        features = "src/test/resources/Features",
+        glue = {"KingsleyGate.StepDef"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-html-reports",
+                "json:target/jsonReports/cucumber.json",
+                "io.cucumber.plugin.Publish"  // 🔗 Publishes to reports.cucumber.io
+        },
+        monochrome = true
+)
 
-        (features = "src/test/resources/Features",
-                glue = {"KingsleyGate.StepDef"},
-                plugin = {
-                        "pretty",
-                        "html:target/cucumber-html-reports",
-                        "json:target/jsonReports/cucumber.json"
-                },
-                monochrome = true)
 
 public class TestRunner extends AbstractTestNGCucumberTests
 {
