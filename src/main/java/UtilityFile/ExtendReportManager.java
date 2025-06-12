3284dfebdbabe1fe5c5bf40705925962a0c77e57
Namrata Mehta <nmehta@kingsleygate.com>
2025-06-12 17:01:15 +0530

    Jenkins changes

--- src/main/java/UtilityFile/ExtendReportManager.java
+++ src/main/java/UtilityFile/ExtendReportManager.java
@@ -25,12 +25,11 @@ public class ExtendReportManager implements ITestListener
 		if(extent == null)
 		{
 		//String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());//time stamp
-		//repName="Igyte_Automation_Report.html";
+		repName="index.html";
 		
-		//String reportPath = System.getProperty("user.dir") + "/ext_reports/" + repName;
-		//sparkReporter = new ExtentSparkReporter(reportPath);
-			
-			sparkReporter = new ExtentSparkReporter("ext_reports");
+		String reportPath = System.getProperty("user.dir") + "/ext_reports/" + repName;
+		sparkReporter = new ExtentSparkReporter(reportPath);
+				
 		//sparkReporter=new ExtentSparkReporter("/ext_reports/"+repName);//specify location of the report
 		System.out.println("Testing extend report execution");		
 		sparkReporter.config().setDocumentTitle("Ignyte Project"); // Title of report
